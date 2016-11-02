61a741bf4fc38c11a01126ba0bb0e9b36f2f4b3d
Angeliki Skandalou <agg.skandalou@gmail.com>
2016-11-02 11:43:13 +0100

    speech bubbles

--- ChivalryIsDead/Assets/Programmers/Angeliki/Scripts/SpeechBubbles.cs
+++ ChivalryIsDead/Assets/Programmers/Angeliki/Scripts/SpeechBubbles.cs
@@ -1,23 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SpeechBubbles : MonoBehaviour {
     public bool needsInput;
     public bool isPaused;
+
+    Text text;
 	// Use this for initialization
 	void Start () {
+        //text = GetComponentInChildren(Text);
+
+        // stuff for pausing
         Time.timeScale = 0;
         isPaused = true;
     }
 
     // Update is called once per frame
     void Update () {
+        // stuff for pausing
         if (needsInput)
         {
             StartCoroutine(WaitForKeyDown(KeyCode.Mouse0));
         }
     }
 
+    // stuff for pausing
     IEnumerator WaitForKeyDown(KeyCode keyCode)
     {
         while (!Input.GetKeyDown(keyCode))
