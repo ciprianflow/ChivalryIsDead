90507ab92d35934dc3ace7b98074e9fc69f15913
PhilBer <pdgberman@gmail.com>
2016-11-07 15:22:10 +0100

    player can turn buttery smoothlike, action button turn function

---
+++ ChivalryIsDead/Assets/ToggleControlScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ToggleControlScript : MonoBehaviour {
+    void Awake() {
+        Player ps = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        gameObject.GetComponent<Button>().onClick.AddListener(delegate { ps.toggleControls(); });
+    }
+}
