6b3e54b5c55fa55570551e9dc1ad9b5acd9368a4
PhilBer <lechieu@hotmail.com>
2016-11-15 10:21:34 +0100

    Fixes

--- ChivalryIsDead/Assets/Programmers/Samuel/Scripts/MonsterAI.cs
+++ ChivalryIsDead/Assets/Programmers/Samuel/Scripts/MonsterAI.cs
@@ -86,7 +86,7 @@ public abstract class MonsterAI : MonoBehaviour, IObjectiveTarget {
         //HARD CODED REMOVE LATER
         //HARD CODED REMOVE LATER
         //HARD CODED REMOVE LATER
-        Debug.DrawLine(transform.position, GetTargetPosition() + new Vector3(0, 0.5f, 0));
+        Debug.DrawLine(transform.position, GetTargetPosition());
         if (targetObject != null && !targetObject.gameObject.activeSelf)
             targetObject = StaticIngameData.player;
         //HARD CODED REMOVE LATER
