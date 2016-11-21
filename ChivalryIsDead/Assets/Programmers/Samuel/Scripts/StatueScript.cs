41085e56fb8d04b803805f12d1d39a64785655ef
PhilBer <lechieu@hotmail.com>
2016-11-21 14:44:20 +0100

    no message

---
+++ ChivalryIsDead/Assets/Programmers/Samuel/Scripts/StatueScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StatueScript : MonoBehaviour {
+
+    public Sprite[] sprites;
+
+	// Use this for initialization
+	void Start () {
+
+        if (sprites == null || sprites.Length == 0)
+            return;
+
+        float percantage = StaticData.Reputation / 100;
+
+        float tic = 1f / sprites.Length;
+
+        for(int i = 0; i < sprites.Length; i++)
+        {
+            if(tic * i > percantage)
+            {
+                GetComponent<SpriteRenderer>().sprite = sprites[i];
+                break;
+            }
+        }
+
+
+	}
+}
