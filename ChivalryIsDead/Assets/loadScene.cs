ce92f71b91da0a462ad1c3dca32b09cc57d027ce
PhilBer <lechieu@hotmail.com>
2016-11-30 13:19:37 +0100

    no message

---
+++ ChivalryIsDead/Assets/loadScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class loadScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        GetComponent<Text>().text = "Current scene - " + SceneManager.GetActiveScene().name;
+
+	}
+	
+}
