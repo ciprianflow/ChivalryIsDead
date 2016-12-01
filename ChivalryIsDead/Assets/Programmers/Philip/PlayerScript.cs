de3d922c778351bcf24b66fd75e67c7799a02d46
PhilBer <lechieu@hotmail.com>
2016-12-01 15:16:18 +0100

    no message

--- ChivalryIsDead/Assets/Programmers/Philip/PlayerScript.cs
+++ ChivalryIsDead/Assets/Programmers/Philip/PlayerScript.cs
@@ -392,11 +392,13 @@ public class PlayerScript : MonoBehaviour {
 
         //Debug.Log(ASI.normalizedTime / ASI.length);
 
+
+
         if (ASI.IsTag("Attack1"))
         {
             currentAttack = 2;
             int r = (int)UnityEngine.Random.Range(0, 3);
-            if (r == 0 || SceneManager.GetActiveScene().name == "IntroLevel") {
+            if (r == 0 || SceneManager.GetActiveScene().name == "IntroLevel" || PlayerPrefs.GetInt("AttackLevel") == 0) {
                 anim.SetTrigger("Attack2");
             }
             else if(r == 1) {
@@ -410,7 +412,7 @@ public class PlayerScript : MonoBehaviour {
         {
             currentAttack = 1;
             int r = (int)UnityEngine.Random.Range(0, 3);
-            if (r == 0 || SceneManager.GetActiveScene().name == "IntroLevel") {
+            if (r == 0 || SceneManager.GetActiveScene().name == "IntroLevel" || PlayerPrefs.GetInt("AttackLevel") == 0) {
                 anim.SetTrigger("Attack1");
             }
             else if (r == 1) {
