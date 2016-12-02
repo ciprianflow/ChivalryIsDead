afe9cee237227769073796530b68aecc8cb74138
PhilBer <lechieu@hotmail.com>
2016-12-02 11:26:53 +0100

    no message

---
+++ ChivalryIsDead/Assets/setAR.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class setAR : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AspectUtility.SetCamera();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
