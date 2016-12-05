92c49c5bf3d762cfe80cdad49c72211b945446b1
PhilBer <lechieu@hotmail.com>
2016-12-05 09:39:07 +0100

    no message

---
+++ ChivalryIsDead/Assets/CinematicTextEvents.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CinematicTextEvents : MonoBehaviour {
+
+    public Renderer textMat;
+    public Texture tex;
+    public List<Texture> speechBubbles;
+
+    int curTex = -1;
+	// Use this for initialization
+	void Start () {
+        //textMat.material.SetTexture("_MainTex", tex);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void nextText() {
+        curTex++;
+        textMat.material.SetTexture("_MainTex", speechBubbles[curTex]);
+    }
+}
