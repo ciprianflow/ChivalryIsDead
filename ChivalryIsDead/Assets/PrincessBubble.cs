2f52ec05442a91e33a8193a51d124edbdf1b5cbd
PhilBer <lechieu@hotmail.com>
2016-12-05 14:15:06 +0100

    no message

---
+++ ChivalryIsDead/Assets/PrincessBubble.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrincessBubble : MonoBehaviour {
+
+    public Texture DanishBubble;
+    public Renderer textMat;
+    // Use this for initialization
+    void Awake () {
+        if (PlayerPrefs.GetString("Language") == "Dansk" || 1 == 1) {
+            textMat.material.SetTexture("_MainTex", DanishBubble);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
