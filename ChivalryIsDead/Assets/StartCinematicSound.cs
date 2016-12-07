5aa9fb026d1a1bc2dba5a7523cb565141e8df546
PhilBer <lechieu@hotmail.com>
2016-12-07 13:44:08 +0100

    no message

--- ChivalryIsDead/Assets/StartCinematicSound.cs
+++ ChivalryIsDead/Assets/StartCinematicSound.cs
@@ -5,13 +5,10 @@ public class StartCinematicSound : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-<<<<<<< HEAD
         WwiseInterface.Instance.StopEvent("music1Play");
         WwiseInterface.Instance.StopEvent("musicquest");
         WwiseInterface.Instance.StopEvent("start_world_1_ambience");
         WwiseInterface.Instance.StopEvent("reward_combo_start");
-=======
->>>>>>> 4001b32c51a2b315532867989a91cb998200ecd1
 
         WwiseInterface.Instance.SetMusic(MusicHandle.MusicStop);
         AkSoundEngine.PostEvent("cinematicIntro", gameObject);
