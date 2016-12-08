daf84915e3f3da46620ad01d81f6238439489c81
PhilBer <lechieu@hotmail.com>
2016-12-08 14:40:29 +0100

    Fixes.. alot of them

--- ChivalryIsDead/Assets/Hub_Dialog.cs
+++ ChivalryIsDead/Assets/Hub_Dialog.cs
@@ -12,8 +12,6 @@ public class Hub_Dialog : MonoBehaviour {
 	// Use this for initialization
 	IEnumerator Start () {
 
-        
-
         yield return new WaitForSeconds(0.5f);
 
         if (StaticData.Reputation <= 110 && StaticData.Reputation > 100)
